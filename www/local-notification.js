37bf2f8023aeab627d7e521fb643534dfad512d1
Joyce Kim <jkim@healthrecoverysolutions.com>
2020-08-07 12:40:57 -0700

    Remove onload param in plugin.xml.
    The onload param causes clicked notifications to open app home page instead of redirecting to the relevant page.

--- www/local-notification.js
+++ www/local-notification.js
@@ -575,8 +575,8 @@ exports._mergeWithDefaults = function (options) {
     }
 
     options.meta = {
-        plugin:  'cordova-plugin-local-notification',
-        version: '0.9-beta.3'
+        plugin:  'hrs-cordova-plugin-local-notification',
+        version: '0.9-beta.4'
     };
 
     return options;
